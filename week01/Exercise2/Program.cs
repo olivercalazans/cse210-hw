4d09b7723810d8a4246398b51ce329466911ead9
olivercalazans <oliver.calazans@gmail.com>
2026-05-09 14:52:47 -0300

    add exer 1 and 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade: ");
+        string input = Console.ReadLine();
+
+        int grade = int.Parse(input);
+
+        if (grade > 100 || grade < 0)
+        {
+            Console.WriteLine("Your grade must be between 0 and 100");
+            return;
+        }
+        
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int rest = grade % 10;
+        string signal = "";
+
+        if (rest >= 7 && letter != "A" && letter != "F")
+        {
+            signal = "+";
+        }
+        else if (rest < 3 && letter != "F" && grade < 100)
+        {
+            signal = "-";
+        }
+
+        Console.WriteLine($"Your letter is {letter}{signal}");
     }
 }
